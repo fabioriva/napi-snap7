1bd54f221e0d3c0961e6e7fb02c274f150adc149
Fabio Riva <f.riva@sotefin.ch>
2022-04-09 08:05:34 +0200

    fix async client example

--- examples/async.js
+++ examples/async.js
@@ -7,7 +7,7 @@ const time = (ms) => console.log("| Execution time : %d ms", ms);
 const res = client.ConnectTo("192.168.57.12", 0, 1);
 
 client.ReadArea(0x84, 510, 0, 100, 0x02, (err, data) => {
-  if (err) return console.log(snap7.CliErrorText(err));
+  if (err) return console.log(client.ErrorText(err));
   console.log("Data", data);
   time(client.ExecTime());
   client.Disconnect();
